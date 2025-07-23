c5e100f374dd39a9eb78b2ebab8fb53ace084864
MrJoyner95 <oct_romo@hotmail.com>
2025-07-23 17:07:51 -0600

    Getting repo files

--- core/main.py
+++ core/main.py
@@ -5,18 +5,22 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain.agents import create_tool_calling_agent, AgentExecutor
-from tools import  save_tool
+from tools import save_tool
 import chromadb
 
 client = chromadb.HttpClient(host="localhost", port=8000, ssl=False)
-
+from tools import get_repo_files
 from typing import Union
-
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
-
 load_dotenv()
+
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++ LangChain Agents ++++++++++++++++++++++++++++++++++++++++
 
 class ResearchResponse(BaseModel):
     topic: str
@@ -25,7 +29,6 @@ class ResearchResponse(BaseModel):
     additional_info: str
     #sources: list[str]
     #tools_used: list[str]
-    
 
 llm = ChatAnthropic(model="claude-3-5-sonnet-20241022")
 parser = PydanticOutputParser(pydantic_object=ResearchResponse)
@@ -46,7 +49,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
-tools = [ ]
+tools = [save_tool]
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
@@ -55,11 +58,48 @@ agent = create_tool_calling_agent(
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++ Fast API ++++++++++++++++++++++++++++++++++++++++
+
 @app.post("/documentation")
 def create_documentation(query: str):
     raw_response = agent_executor.invoke({"query": query})
-
-
     structured_response = parser.parse(raw_response.get("output")[0]["text"])
     return structured_response
+
+
+# Repo Files Processing
+@app.post("/getRepo")
+def get_files_list(repo_url: str):
+    files = get_repo_files(repo_url)
+    print(files)
+    # Loops files, reads them and sends to agents:
+    for file in files:
+        try:
+            # file_path = os.path.join(root, file)
+            file_path = file
+            with open(file_path, 'r', encoding='utf-8') as f:
+                file_content = f.read()
+                # print("file_content:")
+                # print(file_content)
+                raw_response = agent_executor.invoke({"query": file_content})
+                structured_response = parser.parse(raw_response.get("output")[0]["text"])
+                print(structured_response)
+                # print(f"\n--- {file_path} ---\n")
+                # print(f.read())
+        except Exception as e:
+            print(f"\n--- Skipped {file_path} (error: {e}) ---\n")
+
+    return {"files": files}
+
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++ Repo Files Processing ++++++++++++++++++++++++++++++++++++++++
+
+
+
+
 
--- core/tools.py
+++ core/tools.py
@@ -4,17 +4,20 @@ from langchain.tools import Tool
 from datetime import datetime
 
 def save_to_txt(data: str, filename: str = "research_output.txt"):
+    os.makedirs("docs", exist_ok=True)
+    full_path = os.path.join("docs", filename)
+
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     formatted_text = f"--- Research Output ---\nTimestamp: {timestamp}\n\n{data}\n\n"
 
-    with open(filename, "a", encoding="utf-8") as f:
+    with open(full_path, "a", encoding="utf-8") as f:
         # erase the file if it exists
         f.truncate(0)
         
         # write the formatted text
         f.write(formatted_text)
     
-    return f"Data successfully saved to {filename}"
+    return f"Data successfully saved to {full_path}"
 
 save_tool = Tool(
     name="save_text_to_file",
@@ -22,15 +25,38 @@ save_tool = Tool(
     description="Saves structured research data to a text file.",
 )
 
- 
 
-# search = DuckDuckGoSearchRun()
-# search_tool = Tool(
-#     name="search",
-#     func=search.run,
-#     description="Search the web for information",
-# )
+# Getting files from a repo:
+import os
+import subprocess
+import shutil
 
-# api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=100)
-# wiki_tool = WikipediaQueryRun(api_wrapper=api_wrapper)
+def get_repo_files(repo_url: str, clone_dir: str = "repo"):
+    # Clears the clone directory:
+    if os.path.exists(clone_dir):
+        shutil.rmtree(clone_dir)
+
+    # Clones repository
+    subprocess.run(["git", "clone", repo_url, clone_dir], check=True)
+
+    # Loops files and returns list:
+    files_list = []
+    for root, _, files in os.walk(clone_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            # Skips hidden files:
+            if file_path.startswith("repo/."):
+                continue
+            files_list.append(file_path)
+            # print(file_path)
+            # Reads files content
+            # try:
+            #     with open(file_path, 'r', encoding='utf-8') as f:
+            #         print(f"\n--- {file_path} ---\n")
+            #         print(f.read())
+            # except Exception as e:
+            #     print(f"\n--- Skipped {file_path} (error: {e}) ---\n")
+    
+    # print(files_list)
+    return files_list
 
